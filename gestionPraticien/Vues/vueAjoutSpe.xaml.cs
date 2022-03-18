7a917144bc11c97eb6ee8b1a0d3124165ce99bc2
Tristan Blin <tristanblin21@gmail.com>
2022-03-18 09:12:40 +0100

    Ajout de commentaires à vueAjoutSpe.cs

--- gestionPraticien/Vues/vueAjoutSpe.xaml.cs
+++ gestionPraticien/Vues/vueAjoutSpe.xaml.cs
@@ -22,33 +22,38 @@ namespace gestionPraticien.Vues
     public partial class vueAjoutSpe : Window
     {
         private GstBDD gst;
-
         public GstBDD Gst { get => gst; set => gst = value; }
 
         public vueAjoutSpe(GstBDD unGst)
         {
+            //On récupère le gst passé en paramètre
             Gst = unGst;
             InitializeComponent();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // Alimentation de la liste des spécialités
             lbDesSpe.ItemsSource = Gst.GetLesSpecialite();
         }
 
         private void btnModifier_Click(object sender, RoutedEventArgs e)
         {
+            //On vérifie si l'utilisateur à cliquer sur une spécialité dans la liste
             if (lbDesSpe.SelectedItem == null)
             {
                 MessageBox.Show("Veuillez sélectionner une spécialité ", "Erreur de sélection", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            // On vérifie si l'utilisateur à mis un nom à la spécialité qu'il veut modifier
             else if(txtModifSpe.Text == "")
             {
                 MessageBox.Show("Veuillez entrer un nom ", "Erreur de nom", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else
             {
+                //On met à jour la spécialité sélectionnée
                 gst.UpdateSpe((lbDesSpe.SelectedItem as Specialite).IdSpe , txtModifSpe.Text);
+                //On alimente la liste des spécialités suite à la modification
                 lbDesSpe.ItemsSource = Gst.GetLesSpecialite();
 
             }
@@ -56,21 +61,26 @@ namespace gestionPraticien.Vues
         
         private void btnAjouter_Click(object sender, RoutedEventArgs e)
         {
+            //On vérifie si l'utilisateur à rentrer un nom à la spécialité qu'il veut ajouter
             if (txtNomSpe.Text == "")
             {
                 MessageBox.Show("Veuillez entrer un nom ", "Erreur de nom", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else
             {
+                //On ajoute la spécialité
                 gst.AjouterSpe(txtNomSpe.Text);
+                // On alimente la liste des spécialités suite à l'ajout
                 lbDesSpe.ItemsSource = Gst.GetLesSpecialite();
             }
         }
 
         private void lbDesSpe_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {   
+        {
+            //On vérifie si l'utilisateur à cliquer sur une spécialité dans la liste
             if (lbDesSpe.SelectedItem != null)
             {
+                //On met à jour le texte du nom de la spécialité selectionnée
                 txtModifSpe.Text = ((lbDesSpe.SelectedItem as Specialite).NomSpe).ToString();
             }
         }
